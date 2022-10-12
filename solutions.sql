304c2c7c4ceebcc127643bd2400500885617a824
fjalafoo <fjalafoo@gmail.com>
2022-10-12 14:13:17 +0300

    solved 11

--- solutions.sql
+++ solutions.sql
@@ -98,6 +98,12 @@
 -- ### Grouping and Counting
 
 -- 11. Get the COUNT of all books
+    -- SELECT COUNT(id)
+    -- FROM books;
+    --result: 15
+    
 -- 12. Get the COUNT of all Locations
+
 -- 13. Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
